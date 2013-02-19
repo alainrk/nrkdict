dd1fe956e4cb575707742549649c929ee4e16760
alainrk <alain.narko@gmail.com>
2013-02-19 15:23:10 +0100

    Scheleton

---
+++ src/nrkdictGUI/GuiController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nrkdictGUI;
+
+/**
+ *
+ * @author narko
+ */
+
+import nrkdict.*;
+
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author narko
+ */
+public class GuiController {
+    public GuiController(){
+    }
+    public static void createGui(){
+    }
+}
